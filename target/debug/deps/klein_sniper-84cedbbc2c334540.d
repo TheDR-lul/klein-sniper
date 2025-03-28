031c3588ae2c9bd648cfe22e39fd161217d88b4b
WingsOfPrey <antonkalantarenko@gmail.com>
2025-03-28 12:21:50 +0100

    update

---
+++ target/debug/deps/klein_sniper-84cedbbc2c334540.d
@@ -0,0 +1,7 @@
+E:\git\klein-sniper\target\debug\deps\libklein_sniper-84cedbbc2c334540.rmeta: src\main.rs src\config.rs src\model.rs
+
+E:\git\klein-sniper\target\debug\deps\klein_sniper-84cedbbc2c334540.d: src\main.rs src\config.rs src\model.rs
+
+src\main.rs:
+src\config.rs:
+src\model.rs:
